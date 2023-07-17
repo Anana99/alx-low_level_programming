72061d8ecd8579e97eb04f810269421c39f8dda6
Anana99 <ananaitohoinyene99@gmail.com>
2023-07-17 10:00:00 -0700

    C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print name of file
+ *        followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
